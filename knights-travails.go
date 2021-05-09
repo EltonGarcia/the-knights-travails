8b87e281f658abcaf65b411690c0047a2c8de006
Elton Santana <elton.garcia.s@gmail.com>
2021-05-09 14:04:15 -0300

    add algorithm scaffold

--- knights-travails.go
+++ knights-travails.go
@@ -29,8 +29,30 @@ var moves = []Move{
 	{ 2, 1 },
 }
 
-func findMoves(start, target Square){
 
+// findMoves - find the shortest sequence of valid moves
+// to take a knight from the start to the target position.
+func findMoves(start, target Square){
+	// Enqueue the start position.
+
+	// Use a map to keep track of the visited squares.
+
+	// Loop through the queue until it is empty.
+
+	// Pop an element, onwards called current
+
+	// Check if target was already found and distance is
+	// greater than the shortest distance
+
+	// Check if current is the target square, and
+	// update the shortest distance and target found flag.
+
+	// Loop through moves.
+
+	// Apply each move to the current square.
+
+	// Check if the result square was visited.
+	// Enqueue if not.
 }
 
 func main(){
